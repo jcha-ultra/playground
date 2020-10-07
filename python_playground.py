c5feb8f686c4ab289c98e9a8546d3e1b2a54ef01
jcha-ultra <john@ultratesting.us>
2020-10-07 11:53:18 -0400

    Initial version!

--- python_playground.py
+++ python_playground.py
@@ -4,7 +4,12 @@
 
 
 
-
+import functools
+def compose(*functions):
+    """
+    Returns 
+    """
+    return functools.reduce(lambda f, g: lambda x: f(g(x)), functions, lambda x: x)
 
 
 
@@ -18,3 +23,41 @@
 
 if __name__ == "__main__":
     pass
+    
+    
+    
+    
+
+
+
+# generate_closure: given f(*kwargs), create g(*kwargs) where g generates a closure for f based on what args were supplied
+# generating contexts: exec_with_context(f, context), where context is a dict of kwargs
+# inspect.getsource(function)
+# memoizing
+###
+# functools
+# itertools
+###
+# code from scratch.py
+# code from records 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
